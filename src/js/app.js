a6314ad72e563e877de1856fd619ada797dcdb5a
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-11 21:52:03 +0900

    화면에 username 노출하기

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,28 @@
+
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button")
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"
 
 function onLoginSubmit(e) {
     e.preventDefault();
     const username = loginInput.value;
     if(!username){
         alert("이름을 입력해주세요")
+        return false;
     }else if(username.length > 15){
         alert("이름은 10자 이내로 입력해주세요")
+        return false;
     }
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.textContent=`WELCOME ${username}!`;
+
+    if(username){
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+    }
+    return true;
+
 }
 
 loginForm.addEventListener("submit", onLoginSubmit)
+
