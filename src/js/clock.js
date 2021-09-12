7a5bd69c92f895b1ef26d393c4e5e01a71b70bbf
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-12 22:54:34 +0900

    clock 완성

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,8 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-// clock.textContent=""
-function sayHello(){
-    console.log("hello")
+const getClock = () => {
+    const getDate = new Date();
+    const hours = String(getDate.getHours()).padStart(2, "0");
+    const minutes = String(getDate.getMinutes()).padStart(2, "0");
+    const seconds = String(getDate.getSeconds()).padStart(2, "0");
+    clock.textContent=`${hours} : ${minutes} : ${seconds}`;
 }
 
-setInterval(sayHello, 1000)
+getClock();
+setInterval(getClock, 1000);
