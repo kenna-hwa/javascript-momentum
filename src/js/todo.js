573d6d291dad0cdbdd3bd44b566cffbb0f12dbf1
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-14 00:35:41 +0900

    JSON strigify 사용

--- src/js/todo.js
+++ src/js/todo.js
@@ -2,7 +2,11 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+const toDos = [];
 
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos))
+}
 
 function deleteTodo(event){
     const listItem = event.target.parentElement;
@@ -26,7 +30,9 @@ function handleTodoSubmit(event){
     event.preventDefault();
     const newtodo = todoInput.value;
     todoInput.value = "";
+    toDos.push(newtodo);
     paintToDo(newtodo);
+    saveToDos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
